1b85a75ef9de396d411162c543ad25d086c20dc7
natalia <natager1503@gmail.com>
2025-06-17 18:08:19 +0000

    Лабораторна 4. Адмінка категорій

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], 
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
 Route::resource('rest', RestTestController::class)->names('restTest');
+
+//Адмінка
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
