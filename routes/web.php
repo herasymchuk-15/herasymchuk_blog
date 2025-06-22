830c43f18a85820d46da2f5e2403a86a60f4fce5
natalia <natager1503@gmail.com>
2025-06-22 15:31:29 +0000

    Лабораторна 15. Vue, API

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Route::middleware([
 Route::resource('rest', RestTestController::class)->names('restTest');
 
 Route::group(['prefix' => 'digging_deeper'], function () {
+
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
         ->name('digging_deeper.collections');
 
     Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
@@ -31,16 +33,15 @@ Route::group(['prefix' => 'digging_deeper'], function () {
         ->name('digging_deeper.prepareCatalog');
 });
 
-
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
+
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
-
 Route::group($groupData, function () {
     //BlogCategory
     $methods = ['index','edit','store','update','create',];
